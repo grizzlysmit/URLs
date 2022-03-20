008af2ff0c92bc6f21462b27c3a2b914c261be13
Francis Grizzly Smit <grizzly@smit.id.au>
2022-03-21 00:03:50 +1100

    commit home page 2022-03-21T00:03:50

---
+++ web/Urls.pm
@@ -0,0 +1,74 @@
+#
+#===============================================================================
+#
+#         FILE: Urls.pm
+#
+#  DESCRIPTION: 
+#
+#        FILES: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Francis Grizzly Smit (FGJS), grizzly@smit.id.au
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 2022-03-20 22:57:02
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use v5.32.1;
+use Apache2::Request;
+use Data::Dumper;
+use FileHandle;
+use Config::IniFiles;
+use Class::Std::Utils;
+use JSON;
+use JSON::XS;
+use DBI;
+
+{
+    my %logpaths;
+    my %db_handle;
+    my %debug;
+    my %logfiles;
+
+    
+    sub new {
+        my ($class, $logpath, $cfg) = @_;
+        #
+        # make the actual object a so called blessed scalar #
+        # slightly quirky but it works :)                   #
+        my $new_object = bless anon_scalar(), $class;
+
+        # get the instances idnet for referencing it's data #
+        my $ident = ident $new_object;
+
+        $logpaths{$ident} = $logpath;
+
+        return $new_object;
+    } ## --- end sub new
+    
+    sub debug_init {
+        my ($self, $_debug, $_log) = @_;
+        my $ident = ident $self;
+
+        $debug{$ident} = $_debug;
+
+        $logfiles{$ident} = $_log;
+    } # sub debug_init #
+
+    sub log {
+        my $self = shift;
+        my $ident = ident $self;
+        return unless $debug{$ident};
+        my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);
+        $year += 1900;
+        $mon++;
+        my $timestamp = sprintf "%04d-%02d-%02dT%02d:%02d:%02d\t", $year, $mon, $mday, $hour, $min, $sec;
+        say {$logfiles{$ident}} $timestamp, @_; 
+    } ## --- end sub log
+
+}
+
+1;
