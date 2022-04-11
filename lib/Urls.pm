095cbe55e527ac7e504a80d9380812c30938529f
Francis Grizzly Smit <grizzly@smit.id.au>
2022-04-11 16:06:21 +1000

    commit home page 2022-04-11T16:06:21

--- lib/Urls.pm
+++ lib/Urls.pm
@@ -2723,8 +2723,9 @@ use Crypt::URandom;
                 my $email             = $r->{_email};
                 my $phone_number      = $r->{phone_number};
                 my $groupname        = $r->{groupname};
+                $self->log(Data::Dumper->Dump([$r], [qw(r)]));
 
-                $self->log(Data::Dumper->Dump([$username, $password, $hashed_password], [qw(username password hashed_password)]));
+                $self->log(Data::Dumper->Dump([$result, $username, $password, $hashed_password], [qw(result username password hashed_password)]));
                 if($self->validate($hashed_password, $password)){
                     $session{loggedin}               = $loggedin_id;
                     $session{loggedin_id}            = $loggedin_id;
