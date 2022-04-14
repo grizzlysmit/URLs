6833b5eb258202d60a2241080e9e5df51fe289ef
Francis Grizzly Smit <grizzly@smit.id.au>
2022-04-14 13:19:59 +1000

    commit home page 2022-04-14T13:19:59

--- sql/select-perms.sql
+++ sql/select-perms.sql
@@ -1,1 +1,5 @@
-SELECT * FROM links WHERE  id = 1 AND (1 = 1 OR (userid = 1 AND (_perms)._user._read = true) OR (groupid = 1 AND (_perms)._group._read = true) OR (_perms)._other._read = true)
+-- UPDATE links SET _perms._other._read = true WHERE userid = 1;
+SELECT * FROM links z WHERE  -- z.id = 1 AND 
+                             (1 = 1 OR (z.userid = 1 AND (z)._perms._user._read = true) 
+										   OR ((z.groupid = 1 OR z.groupid IN (SELECT gs.group_id FROM groups gs WHERE gs.passwd_id = 1)) AND (z)._perms._group._read = true) 
+										   OR (z)._perms._other._read = true)
