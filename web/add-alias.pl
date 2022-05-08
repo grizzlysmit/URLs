d7cdb49d869eac392a994d8c4dcc1ed1c1a599ca
Francis Grizzly Smit <grizzly@smit.id.au>
2022-05-09 00:40:44 +1000

    commit home page 2022-05-09T00:40:44

--- web/add-alias.pl
+++ web/add-alias.pl
@@ -66,7 +66,7 @@ say <<"END";
     <head>
         <title>Grizzly&apos;s links page</title>
         <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
-        <link rel="stylesheet" href="styles/styles.css">
+        <link rel="stylesheet" href="/styles/styles.css">
     </head>
     <body>
 END
