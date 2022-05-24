1dd7fc3f91a1067bd7be0231ee8a658cd062fce3
Francis Grizzly Smit <grizzly@smit.id.au>
2022-05-24 21:52:30 +1000

    commit home page 2022-05-24T21:52:30

--- lib/Urls.pm
+++ lib/Urls.pm
@@ -7619,8 +7619,8 @@ use HTML::Entities;
         $mobile_placeholder   = '' unless defined $mobile_placeholder;
         $list                 = '' unless defined $list;
 
-        $self->log(Data::Dumper->Dump([$cc, $rprefix, $name, $flag, $landline_pattern, $mobile_pattern, $landline_title, $mobile_title, $landline_placeholder, $mobile_placeholder, $list],
-                [qw(cc rprefix name flag landline_pattern mobile_pattern landline_title mobile_title landline_placeholder mobile_placeholder cc)]));
+        $self->log(Data::Dumper->Dump([$cc, $country_prefix, $name, $flag, $landline_pattern, $mobile_pattern, $landline_title, $mobile_title, $landline_placeholder, $mobile_placeholder, $list],
+                [qw(cc country_prefix name flag landline_pattern mobile_pattern landline_title mobile_title landline_placeholder mobile_placeholder cc)]));
 
         untie %session;
         $db->disconnect;
